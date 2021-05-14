36d7edcf9c6b794bc2861d99a26c734a290b3ce8
leonardo.goes <leonardo.goes@luizalabs.com>
2021-05-14 11:43:05 -0300

    Add project files.

---
+++ AppMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using ActionResult = System.Web.Mvc.ActionResult;
+using Controller = System.Web.Mvc.Controller;
+
+namespace AppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UsingIFrame()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult UsingExternalApplicationRoute()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }       
+        public ActionResult UsingInjectById()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }       
+        public ActionResult UsingWebComponents()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
